4d472768c2979d833e543043bca7e198766dcaf5
Pierre Tardy <tardyp@gmail.com>
2017-05-03 19:12:07 +0200

    dostress

--- marathon/ci_perf/dostress.sh
+++ marathon/ci_perf/dostress.sh
@@ -1,14 +1,14 @@
 # -l/bin/bash
 #python marathon.py master 1
 #sleep 300
-for masters in 1 2 3 4
+for masters in 2 1 3 4
 do
 python stressmarathon.py -f 1 1 $masters restart 1 1 || exit
 for sleeptime in 0
 do
-for numlines in 10000 40000 100
+for numlines in 40000 10000 100
 do
-for workers in 11 51 101 201 291 
+for workers in 201 11 51 101 291 1
 do
 python stressmarathon.py 300 $workers $masters pypy $numlines $sleeptime || exit
 done
